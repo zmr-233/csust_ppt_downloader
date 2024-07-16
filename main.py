44c4cf139bd710a9cb2c87585b350993db6840c7
zmr-233 <132034425+zmr-233@users.noreply.github.com>
2024-07-17 00:11:41 +0800

    修复热键监听
    修复热键监听器未正确初始化，主循环中未正确处理热键监听，程序在未等待用户输入的情况下立即退出

--- main.py
+++ main.py
@@ -14,19 +14,17 @@ crop_box = (243, 0, 1678, 1080)
 
 print("按下回车键开始循环，再次按下回车键结束循环。")
 
-
 def start_stop():
     global running
-
     running = not running
     if running:
         print("循环开始")
     else:
         print("循环结束")
 
-
 keyboard.add_hotkey('enter', start_stop)
 
+# 主循环
 while True:
     if running:
         # 全屏截图并裁剪
@@ -38,16 +36,8 @@ while True:
         # 模拟鼠标单击屏幕中央
         pyautogui.click(x=pyautogui.size().width // 2, y=pyautogui.size().height // 2)
 
-        # 执行间隔，可以调快点但不建议调太快
+        # 执行间隔
         time.sleep(1)
-
-    # 按下回车键结束循环
-    if not running:
+    else:
+        # 如果未运行，则短暂休眠以降低CPU使用率
         time.sleep(0.1)
-        break
-
-    # 检查是否按下了回车键
-    if keyboard.is_pressed('enter'):
-        start_stop()
-
-print("程序已结束。")
